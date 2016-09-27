3b19f82212978c3f9aaf9ccf1cdb4f852b24a750
kakakirbz <thefourth006@gmail.com>
2016-09-27 14:16:50 +0800

    Lahi nasd ni

--- home.php
+++ home.php
@@ -1,6 +1,9 @@
 <html>
+<head>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+
 <body>
 </body>
-	<button class='btn btn-danger'></button>
+	<button class='btn btn-danger'><span class='glyphicon glyphicon-user'></span></button>
 </html>
