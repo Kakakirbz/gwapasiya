99b5fc30d2a80d97485e4591b22798129d3c37a7
sexcstella <sexcstella@gmail.com>
2016-09-27 14:23:23 +0800

    Mao nani siya Na lahi na

--- home.php
+++ home.php
@@ -2,5 +2,5 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <body>
 </body>
-	<button class='btn btn-danger'></button>
+	<button class='btn btn-danger'><i class='glyphicon glyphicon-remove'></i></button>
 </html>
