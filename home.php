f1abadb636bd5ba51b5ee12d984adb7dbe64e1ce
kakakirbz <thefourth006@gmail.com>
2016-09-27 13:46:04 +0800

    Anotha One!

---
+++ home.php
@@ -0,0 +1,5 @@
+<html>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<body>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 	for ($x = 10; $x > 0; $x--) {
 		echo "This is a line";
 	}
+	include("home.php");
 ?>
